a8aafbb985090898dafb2a2f1028d3e57585334e
darkping-dev11 <omerasargd@gmail.com>
2026-04-06 22:13:45 +0300

    Create list.js

---
+++ pages/list.js
@@ -0,0 +1,9 @@
+export default function List() {
+  return `
+    <div class="container">
+      <div class="sidebar" id="levelList"></div>
+      <div class="main" id="levelDetail"></div>
+      <div class="records" id="records"></div>
+    </div>
+  `;
+}
