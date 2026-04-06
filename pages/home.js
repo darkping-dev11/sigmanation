9b1cdb6ebd4ce66316b159a5439595e279b20587
darkping-dev11 <omerasargd@gmail.com>
2026-04-06 22:37:20 +0300

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return `
-    <div style="padding:40px;">
-      <h1>Ana Sayfa</h1>
-    </div>
+    <section class="text-center mt-16 px-6">
+      <p class="text-sm text-gray-400 mb-3">Türkiye'nin #1 Geometry Dash Topluluğu</p>
+      <h1 class="text-6xl font-extrabold mb-6">SIGMANATION</h1>
+      <p class="text-gray-500">Rekorları kır, efsane ol.</p>
+    </section>
   `;
 }
